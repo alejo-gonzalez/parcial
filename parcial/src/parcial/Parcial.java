78cd580c0d2cf462e0867c3b9939a2e8ea6ce4da
sergiodiazp45 <sergio.diazp45@gmail.com>
2013-09-18 10:55:45 -0500

    Anexo suma de vocales y consonantes

--- parcial/src/parcial/Parcial.java
+++ parcial/src/parcial/Parcial.java
@@ -1,4 +1,4 @@
-
+﻿
 package parcial;
 
 import java.util.Scanner;
@@ -12,7 +12,7 @@ public class Parcial {
         System.out.println("INGRERSE EL TEXTO: ");
         String cadena=sc.nextLine ();
         int a=0,e=0,i=0,o=0,u=0,vocales =0;
-        int b=0,c=0,d=0,f=0,g=0,h=0,j=0,k=0,l=0,m=0,n=0,ñ=o,p=0,q=0,r=0,s=0,t=0,v=0,w=0,x=0,y=0,z=0,consonantes=0;
+        int b=0,c=0,d=0,f=0,g=0,h=0,j=0,k=0,l=0,m=0,n=0,ñ=o,p=0,q=0,r=0,s=0,t=0,v=0,w=0,x=0,y=0,z=0;
         int I,J;
         for (I=0;I<cadena.length();I++){
             if (cadena.charAt(I)=='a'||cadena.charAt(I)=='A')
@@ -29,11 +29,77 @@ public class Parcial {
             else
                if (cadena.charAt(I)=='u'||cadena.charAt(I)=='U')
                 u++;
+if (cadena.charAt(I)=='b'||cadena.charAt(I)=='B')
+            b++;
             else
-                if(cadena.charAt(I)!=' ')
-                consonantes++;
+            if (cadena.charAt(I)=='c'||cadena.charAt(I)=='C')
+            c++;
+            else
+            if (cadena.charAt(I)=='d'||cadena.charAt(I)=='D')
+            d++;
+            else
+            if (cadena.charAt(I)=='f'||cadena.charAt(I)=='F')
+            f++;
+            else
+            if (cadena.charAt(I)=='g'||cadena.charAt(I)=='G')
+            g++;
+            else
+            if (cadena.charAt(I)=='h'||cadena.charAt(I)=='H')
+            h++;
+            else
+            if (cadena.charAt(I)=='j'||cadena.charAt(I)=='J')
+            j++;
+            else
+            if (cadena.charAt(I)=='k'||cadena.charAt(I)=='K')
+            k++;
+            else
+            if (cadena.charAt(I)=='l'||cadena.charAt(I)=='L')
+            l++;
+            else
+            if (cadena.charAt(I)=='m'||cadena.charAt(I)=='M')
+            m++;
+            else
+            if (cadena.charAt(I)=='n'||cadena.charAt(I)=='N')
+            n++;
+            else
+            if (cadena.charAt(I)=='ñ'||cadena.charAt(I)=='Ñ')
+            ñ++;
+            else
+            if (cadena.charAt(I)=='p'||cadena.charAt(I)=='P')
+            p++;
+            else
+            if (cadena.charAt(I)=='q'||cadena.charAt(I)=='Q')
+            q++;
+            else
+            if (cadena.charAt(I)=='r'||cadena.charAt(I)=='R')
+            r++;
+            else
+            if (cadena.charAt(I)=='s'||cadena.charAt(I)=='S')
+            s++;
+            else
+            if (cadena.charAt(I)=='t'||cadena.charAt(I)=='T')
+            t++;
+            else
+            if (cadena.charAt(I)=='v'||cadena.charAt(I)=='V')
+            v++;
+            else
+            if (cadena.charAt(I)=='w'||cadena.charAt(I)=='W')
+            w++;
+            else
+            if (cadena.charAt(I)=='x'||cadena.charAt(I)=='X')
+            x++;
+            else
+            if (cadena.charAt(I)=='y'||cadena.charAt(I)=='Y')
+            y++;
+            else
+            if (cadena.charAt(I)=='z'||cadena.charAt(I)=='Z')
+            z++;
+1
+          
         }
+ 
         int suma=a+e+i+o+u;
+        int consonantes=b+c+f+g+h+j+k+l+m+n+ñ+p+q+r+s+t+v+w+x+y+z;
         System.out.println("vocales : "+suma);
         System.out.println("consonantes :"+consonantes);
     }
